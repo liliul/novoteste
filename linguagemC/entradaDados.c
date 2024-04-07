cda0f8a0cc07548185663ed6e41965260e90593d
liliul <liliuestudo@gmail.com>
2024-04-07 14:41:06 -0300

    feat: entradaDados linguagem C

---
+++ linguagemC/entradaDados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int num1 = 0;
+
+int main() {
+    printf("Entrada de Dados com scanf\n\n");
+
+    printf("Qual numero vem na sou mente agora?\n");
+    scanf("%d", &num1);
+
+    printf("O numero é: %d \n", num1);
+}
