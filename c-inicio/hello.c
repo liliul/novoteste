46dd4b39e00fb20223e8e8d7f2737d5ecf97e746
liliul <liliuestudo@gmail.com>
2024-04-07 12:14:53 -0300

    fix: printf do get_int

--- c-inicio/hello.c
+++ c-inicio/hello.c
@@ -1,11 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(int argc,  char *argv[])
+{
     // printf("hello, world\n Naruto E Goku");
     string answer = get_string("what your name: ");
     printf("hello, %s\n", answer);
 
     int age = get_int("Age: ");
-    printf("Age:");
+    printf("Idade: %d\n", age);
 }
