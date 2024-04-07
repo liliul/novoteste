c1c5f634d0e8e8cc47c107e0e6851e7bd2ed55ba
liliul <liliuestudo@gmail.com>
2024-04-07 16:16:35 -0300

    feat: while linguagem C

---
+++ linguagemC/while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // int var1 = 0;
+
+    // while (var1 <= 5) {
+    //     printf("valor da var1: %d \n", var1);
+    //     var1++;
+    // }
+
+    int var2 = 1;
+
+    do {
+        printf("valor de var2: %d \n", var2);
+        var2++;
+    }while (var2 <= 5);
+}
