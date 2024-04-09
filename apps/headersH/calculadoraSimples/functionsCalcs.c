47bcda1bfed13bd3d28c636db0cdb0893d6f8ba0
liliul <liliuestudo@gmail.com>
2024-04-09 13:35:22 -0300

    create: functionsCalcs.h e functionsCalcs.c calaculadoraSimples apps

---
+++ apps/headersH/calculadoraSimples/functionsCalcs.c
@@ -0,0 +1,16 @@
+int mais(int num1 , int num2) {
+    int somar = (num1 + num2);
+    return somar;
+}
+int menos(int num1, int num2) {
+    int somar = num1 - num2;
+    return somar;
+}
+int multiplicar(int num1, int num2) {
+    int somar = num1 * num2;
+    return somar;
+}
+int divisao(int num1, int num2) {
+    int somar = num1 / num2;
+    return somar;
+}
---
+++ apps/headersH/calculadoraSimples/functionsCalcs.h
@@ -0,0 +1,6 @@
+#include "functionsCalcs.c"
+
+int mais(int num1, int num2);
+int menos(int num1, int num2);
+int multiplicar(int num1, int num2);
+int divisao(int num1, int num2);
