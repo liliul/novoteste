69e293e9300e6ebeeb67d464442437799b3bd4c7
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-04-13 19:09:24 -0300

    feat: idiomas apps

--- apps/idiomas.c
+++ apps/idiomas.c
@@ -1,69 +1,64 @@
-/*
- * OlaMundo - Programa de Saudação 'Avançado'!
- *
- * Data: 30/01/2016
- * Autor: Wolfterro
- *
- */
-
 #include <stdio.h>
 #include <string.h>
 
-#define VERSAO		"1.0"
-#define DATAREL		"30/01/2016"
+#define VERSAO "1.0"
+#define DATAREL "14/04/2024"
 
-#define VAZIO		NULL
+#define VAZIO NULL
 
-#define PORTUGUES 	"Olá, mundo!\n"
-#define INGLES		"Hello, world!\n"
-#define ESPANHOL	"¡Hola, mundo!\n"
-#define FRANCES		"Bonjour tout le monde!\n"
-#define ITALIANO	"Ciao, mondo!\n"
-#define ALEMAO		"Hallo, Welt!\n"
-#define JAPONES		"こんにちは、世界! (Kon'nichiwa, sekai!)\n"
-#define CHINES_TRAD	"世界，你好！(Shìjiè, nǐ hǎo!)\n"
-#define RUSSO		"Привет, мир! (Privet, mir!)\n"
+#define PORTUGUES "Olá, mundo!\n"
+#define INGLES "Hello, world!\n"
+#define ESPANHOL "¡Hola, mundo!\n"
+#define FRANCES "Bonjour tout le monde!\n"
+#define ITALIANO "Ciao, mondo!\n"
+#define ALEMAO "Hallo, Welt!\n"
+#define JAPONES "こんにちは、世界! (Kon'nichiwa, sekai!)\n"
+#define CHINES_TRAD "世界，你好！(Shìjiè, nǐ hǎo!)\n"
+#define RUSSO "Привет, мир! (Privet, mir!)\n"
 
 int escolha;
 
-void olamundo(int escolha) {
+void olamundo(int escolha)
+{
 
-	switch(escolha) {
+	switch (escolha)
+	{
 
-		case 1:
-			printf(PORTUGUES);
-			break;
-		case 2:
-			printf(INGLES);
-			break;
-		case 3:
-			printf(ESPANHOL);
-			break;
-		case 4:
-			printf(FRANCES);
-			break;
-		case 5:
-			printf(ITALIANO);
-			break;
-		case 6:
-			printf(ALEMAO);
-			break;
-		case 7:
-			printf(JAPONES);
-			break;
-		case 8:
-			printf(CHINES_TRAD);
-			break;
-		case 9:
-			printf(RUSSO);
-			break;
-		default:
-			printf("\nOpção inválida! Saindo...\n\n");
-			break;
+	case 1:
+		printf(PORTUGUES);
+		break;
+	case 2:
+		printf(INGLES);
+		break;
+	case 3:
+		printf(ESPANHOL);
+		break;
+	case 4:
+		printf(FRANCES);
+		break;
+	case 5:
+		printf(ITALIANO);
+		break;
+	case 6:
+		printf(ALEMAO);
+		break;
+	case 7:
+		printf(JAPONES);
+		break;
+	case 8:
+		printf(CHINES_TRAD);
+		break;
+	case 9:
+		printf(RUSSO);
+		break;
+	default:
+		printf("\nOpção inválida! Saindo...\n\n");
+		break;
 	}
 }
 
-void menu() {
+void menu()
+{
 	printf("====================\n");
 	printf("OlaMundo! Versão %s\n", VERSAO);
 	printf("====================\n\n");
@@ -86,7 +81,8 @@ void menu() {
 	olamundo(escolha);
 }
 
-void help() {
+void help()
+{
 	printf("Use os seguintes argumentos (sem aspas):\n");
 	printf("----------------------------------------\n\n");
 
@@ -108,7 +104,8 @@ void help() {
 	printf("'--version' ou '-v': Mostra a versão atual do programa.\n\n");
 }
 
-void version() {
+void version()
+{
 	printf("OlaMundo --- Versão: ");
 	printf(VERSAO);
 	printf(" --- Data de lançamento: ");
@@ -116,51 +113,68 @@ void version() {
 	printf("\n");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	if (argv[1] == VAZIO) {
+	if (argv[1] == VAZIO)
+	{
 		menu();
 	}
-	else {
-		if (argc > 2) {
+	else
+	{
+		if (argc > 2)
+		{
 			printf("Erro, muitos argumentos! Use apenas um!\n");
 		}
-		else {
+		else
+		{
 
-			if (0 == strcmp(argv[1], "--pt") || 0 == strcmp(argv[1], "-1")) {
+			if (0 == strcmp(argv[1], "--pt") || 0 == strcmp(argv[1], "-1"))
+			{
 				olamundo(1);
 			}
-			else if (0 == strcmp(argv[1], "--en") || 0 == strcmp(argv[1], "-2")) {
+			else if (0 == strcmp(argv[1], "--en") || 0 == strcmp(argv[1], "-2"))
+			{
 				olamundo(2);
 			}
-			else if (0 == strcmp(argv[1], "--es") || 0 == strcmp(argv[1], "-3")) {
+			else if (0 == strcmp(argv[1], "--es") || 0 == strcmp(argv[1], "-3"))
+			{
 				olamundo(3);
 			}
-			else if (0 == strcmp(argv[1], "--fr") || 0 == strcmp(argv[1], "-4")) {
+			else if (0 == strcmp(argv[1], "--fr") || 0 == strcmp(argv[1], "-4"))
+			{
 				olamundo(4);
 			}
-			else if (0 == strcmp(argv[1], "--it") || 0 == strcmp(argv[1], "-5")) {
+			else if (0 == strcmp(argv[1], "--it") || 0 == strcmp(argv[1], "-5"))
+			{
 				olamundo(5);
 			}
-			else if (0 == strcmp(argv[1], "--de") || 0 == strcmp(argv[1], "-6")) {
+			else if (0 == strcmp(argv[1], "--de") || 0 == strcmp(argv[1], "-6"))
+			{
 				olamundo(6);
 			}
-			else if (0 == strcmp(argv[1], "--jp") || 0 == strcmp(argv[1], "-7")) {
+			else if (0 == strcmp(argv[1], "--jp") || 0 == strcmp(argv[1], "-7"))
+			{
 				olamundo(7);
 			}
-			else if (0 == strcmp(argv[1], "--ch") || 0 == strcmp(argv[1], "-8")) {
+			else if (0 == strcmp(argv[1], "--ch") || 0 == strcmp(argv[1], "-8"))
+			{
 				olamundo(8);
 			}
-			else if (0 == strcmp(argv[1], "--ru") || 0 == strcmp(argv[1], "-9")) {
+			else if (0 == strcmp(argv[1], "--ru") || 0 == strcmp(argv[1], "-9"))
+			{
 				olamundo(9);
 			}
-			else if (0 == strcmp(argv[1], "--help") || 0 == strcmp(argv[1], "-h")) {
+			else if (0 == strcmp(argv[1], "--help") || 0 == strcmp(argv[1], "-h"))
+			{
 				help();
 			}
-			else if(0 == strcmp(argv[1], "--version") || 0 == strcmp(argv[1], "-v")) {
+			else if (0 == strcmp(argv[1], "--version") || 0 == strcmp(argv[1], "-v"))
+			{
 				version();
 			}
-			else {
+			else
+			{
 				printf("Argumento Inválido! Use --help ou -h para ajuda.\n");
 			}
 		}
