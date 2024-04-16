9226263dda5b0bb3f7cf8bfcbc024962ac13f4b5
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-04-16 15:00:13 -0300

    create: argc e argv linguagem c

---
+++ linguagemC/argc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc > 1)
+    {
+        printf("Foram inseridos %d argumentos: ", argc);
+        for (i = 0; i < argc; i++)
+        {
+            printf("%s\n", argv[i]);
+        }
+    }
+    else
+    {
+        printf("Não foram inserido nada");
+    }
+}
