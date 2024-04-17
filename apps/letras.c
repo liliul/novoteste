a935edb1555f29dae0c6c0d6ed0059af14d89e2b
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-04-17 17:47:17 -0300

    create: letras apps

---
+++ apps/letras.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int funcIfElse(int nota);
+
+int main()
+{
+    printf("Nota do Estudante?\n");
+
+    int nota;
+
+    printf("\nDigita a Nota do estudande\n");
+    scanf("%d", &nota);
+
+    funcIfElse(nota);
+}
+
+int funcIfElse(int nota)
+{
+    if (nota >= 1 && nota <= 30)
+    {
+        printf("Estudante tirou E nota: %d\n", nota);
+    }
+    else if (nota >= 31 && nota <= 55)
+    {
+        printf("Estudante tirou D nota: %d\n", nota);
+    }
+    else if (nota >= 56 && nota <= 70)
+    {
+        printf("Estudante tirou C nota: %d\n", nota);
+    }
+    else if (nota >= 71 && nota <= 90)
+    {
+        printf("Estudante tirou B nota: %d\n", nota);
+    }
+    else if (nota >= 91 && nota <= 100)
+    {
+        printf("Estudante tirou A nota: %d\n", nota);
+    }
+    else
+    {
+        printf("Nenhuma Nota");
+    }
+}
