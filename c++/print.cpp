5623972ab4b632179ac691cff3fd6e8dd09d16fb
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-05-04 17:03:18 -0300

    feat: print ifs com maior e menor c++

--- c++/print.cpp
+++ c++/print.cpp
@@ -5,12 +5,22 @@ using namespace std;
 int main()
 {
     int numero1 = 5;
-    int numero2 = 3;
+    float numero2 = 3;
 
     cout << "Entrada & Saida de dados." << endl;
 
-    cin >> numero1;
-    cin >> numero2;
+    cin >> numero1 >> numero2;
 
+    if (numero2 >= numero1)
+    {
+        cout << "Sim o numero2 é maior que numero1" << endl;
+    }
+
+    if (numero1 <= numero2)
+    {
+        cout << "Sim o numero1 é menor que numero2" << endl;
+    }
+
+    cout << endl;
     cout << "O numero1: " << numero1 << " & numero2: " << numero2 << endl;
 }
