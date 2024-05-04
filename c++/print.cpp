d2a43e848ce9ab3cdeeabc696b978024f084d9bb
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-05-04 16:35:44 -0300

    create: print c++

---
+++ c++/print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero = 5;
+    int meio = 3;
+
+    cout << "O numero: " << numero << " esta na posicao: " << meio << endl;
+}
