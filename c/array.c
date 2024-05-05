7112efbe4dcaebcb2a8a26227b5fde13ea5facf6
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-05-05 19:37:06 -0300

    feat: array notasFor c

--- c/array.c
+++ c/array.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int notasFor();
+
 int main(void)
 {
     float notas[5] = {7, 8, 9.5, 9.9, 5.2};
@@ -11,4 +13,24 @@ int main(void)
     printf("notas[2] = %.1f\n", notas[2]);
     printf("notas[3] = %.1f\n", notas[3]);
     printf("notas[4] = %.1f\n", notas[4]);
+
+    notasFor();
+}
+
+int notasFor()
+{
+    printf("\n\nArray com for\n");
+
+    int nota[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Digita a nota\n");
+        scanf("%d", &nota[i]);
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("nota[%d]: %d\n", i, nota[i]);
+    }
 }
