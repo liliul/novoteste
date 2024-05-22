5f73997198fed0508497fb588425f62980cbab11
liliul <liliuestudo@gmail.com>
2024-05-22 12:50:19 -0300

    feat: Preencher no menu apps

--- apps/menu.c
+++ apps/menu.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
 
 void menuPrincipal();
+void Preencher();
 
 int main()
 {
     int menu = 0;
-
 
     do {
         clear();
@@ -16,7 +17,8 @@ int main()
         scanf("%d", &menu);
 
         switch (menu) {
-            case 1: clear();printf("\n\nEntrei no Menu Cadastrar\n\n");delay_output(2000);break;
+            // case 1: clear();printf("\n\nEntrei no Menu Cadastrar\n\n");delay_output(2000);break;
+            case 1: clear();Preencher();delay_output(2000);break;
             case 2: clear();printf("\n\nEntrei no Menu do Jogo\n\n");delay_output(2000);break;
             case 3: clear();printf("\n\nSaindo do Jogo\n\n");delay_output(5000);break;
         }
@@ -32,4 +34,36 @@ void menuPrincipal()
     printf("\n\n1 - Cadrastre o Jogador:\n\n");
     printf("\n\n2 - Entrar no Jogo da Sorte:\n\n");
     printf("\n\n3 - Sair do Jogo:\n\n");
+}
+
+typedef struct CADASTRO
+{
+    int codigoCartela;
+    char nomeCompleto[101];
+    char email[101];
+
+} CADASTRO;
+struct CADASTRO cadastro = {0 , "", ""};
+// struct CADASTRO cadastro;
+
+void Preencher()
+{
+    clear();
+    printf("\n\nCadastre seu Jogodor\n\n");
+
+    //cadastro
+    printf("Cadaster seu Nome Completo\n");
+    fflush(stdin);
+    scanf("%99s" , cadastro.nomeCompleto);
+    fgets(cadastro.nomeCompleto, 101, stdin);
+
+    // email
+    printf("Cadaster seu Email\n");
+    fflush(stdin);
+    scanf("%99s" , cadastro.email);
+    fgets(cadastro.email, 101, stdin);
+
+    printf("\nCadastro feito com Sucesso!\n");
+    delay_output(3000);
+
 }
