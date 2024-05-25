8961c90d89aa433c9a42824221faa0c6f1d35a70
liliul <liliuestudo@gmail.com>
2024-05-25 15:31:02 -0300

    feat: MenuJogoDaSorte menu apps

--- apps/headersH/menu/menuGame.c
+++ apps/headersH/menu/menuGame.c
@@ -1,4 +1,13 @@
+#include <curses.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <locale.h>
+
 
 void menuPrincipal()
 {
@@ -6,4 +15,31 @@ void menuPrincipal()
     printf("\n\n1 - Cadrastre o Jogador:\n\n");
     printf("\n\n2 - Entrar no Jogo da Sorte:\n\n");
     printf("\n\n3 - Sair do Jogo:\n\n");
+}
+
+void MenuJogoDaSorte()
+{
+    int menuOpcao;
+
+    printf("\n\n\n");
+    printf("Selecione um jogo da Sorte\n\n\n\n");
+    printf("[0]    NarutoSorte\n\n");
+    printf("[6]    para sair\n\n");
+    scanf("%i", &menuOpcao);
+
+    if (menuOpcao == 1)
+    {
+        clear();
+
+        printf("Você escolheu o NarutoSorte\n\n");
+        napms(2000);
+        clear();
+        printf("Seja bem vindo ao NarutoSorte");
+
+        // InserirNumerosDigitadosNoNarutoSorte();
+
+        napms(2000);
+
+        // MostrarNumerosDigitadosNoNarutoSorte();
+    }
 }
