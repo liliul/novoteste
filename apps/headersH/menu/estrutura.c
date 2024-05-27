beb6fa23d0d2a4f0c2240b262b4812c7a24b7188
liliul <liliuestudo@gmail.com>
2024-05-27 13:32:44 -0300

    create: arquivo estrutura . h e c menu apps

---
+++ apps/headersH/menu/estrutura.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct NarutoSorte {
+    int numeroSorteados1[60];
+    int numeroSorteados2[60];
+    int numeroDigitados[30];
+    int n1[30], n2[30];
+};
+struct NarutoSorte narutoSorte;
+
+void InserirNumerosDigitadosNoNarutoSorte()
+{
+
+}
