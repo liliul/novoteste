9bb7d754cc2296b5f1e931541adbe160e2a84014
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-06-14 22:37:06 -0300

    update: for go_teste go

--- go/go_teste/for.go
+++ go/go_teste/for.go
@@ -5,12 +5,21 @@ import (
     "os"
 )
 
+// func main() {
+//     var s, sep string
+//     for i := 1; i < len(os.Args); i++ {
+//         s += sep + os.Args[i]
+//         sep = " " 
+//     }
+//     fmt.Println(s)
+    
+// }
+
 func main() {
-    var s, sep string
-    for i := 1; i < len(os.Args); i++ {
-        s += sep + os.Args[i]
-        sep = " " 
+    s, sep := "", " "
+
+    for _, arg := range os.Args[1:] {
+        s += sep + arg
     }
     fmt.Println(s)
-    
 }
