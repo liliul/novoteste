7ecbda64fe35b950628485c2e07c4c89d54e0b64
liliul <liliuestudo@gmail.com>
2025-05-05 19:21:14 -0300

    feat: crud PersonController e migrations crud

--- cSharp/crud/Controllers/PersonsController.cs
+++ cSharp/crud/Controllers/PersonsController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using crud.Data;
+using crud.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace crud.Controllers
 {
@@ -16,6 +18,73 @@ namespace crud.Controllers
         public PersonsController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> AddPerson(Person person)
+        {
+            _appDbContext.DB.Add(person);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(person);
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
+        {
+            var persons = await _appDbContext.DB.ToListAsync();
+
+            return Ok(persons);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Person>> GetPersons(int id)
+        {
+            var persons = await _appDbContext.DB.FindAsync(id);
+
+            if (persons == null) 
+            {
+                return NotFound("Person nao encontrado Get[id]");
+            }
+
+            return Ok(persons);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdatePerson(int id, [FromBody] Person personUpdate)
+        {
+            var persons = await _appDbContext.DB.FindAsync(id);
+
+            if(persons == null) {
+                return NotFound("Person nao encontro Update");
+            }
+
+            _appDbContext.Entry(persons).CurrentValues.SetValues(persons);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return StatusCode(201, persons);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            var persons = await _appDbContext.DB.FindAsync(id);
+
+            if(persons == null) {
+                return NotFound("Person nao encontro Delete");
+            }
+
+            _appDbContext.DB.Remove(persons);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok("Person deletado com sucesso");
         }
     }
 }
