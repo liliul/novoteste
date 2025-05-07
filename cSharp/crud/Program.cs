7a9d89e38b52d3a18e559d16f655694bb41ee511
liliul <liliuestudo@gmail.com>
2025-05-07 11:21:24 -0300

    add: cors na api crud

--- cSharp/crud/Program.cs
+++ cSharp/crud/Program.cs
@@ -5,11 +5,19 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// --- Configuração do CORS ---
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", // Nome da sua política
+        builder => builder.WithOrigins("http://localhost:8080") // A ORIGEM do seu frontend
+            .AllowAnyMethod() // Permite todos os métodos HTTP (GET, POST, PUT, DELETE, etc.)
+            .AllowAnyHeader()); // Permite todos os cabeçalhos na requisição
+});
 
 var connectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
@@ -26,6 +34,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// --- Adicionar o Middleware de CORS ---
+app.UseCors("AllowSpecificOrigin");
 
 app.MapControllers();
 
